a331d76d2bcc3f4b464c9b8903bfc46517f3d175
Dan Klos <dsklos@gmail.com>
2016-01-19 09:24:18 -0800

    parses scientific notation

--- src/parse.js
+++ src/parse.js
@@ -25,11 +25,21 @@ Lexer.prototype.isNumber = function(ch) {
 Lexer.prototype.readNumber = function() {
   var number = '';
   while (this.index < this.text.length) {
-    var ch = this.text.charAt(this.index);
+    var ch = this.text.charAt(this.index).toLowerCase();
     if (ch === '.' || this.isNumber(ch)) {
       number += ch;
     } else {
-      break;
+      var nextCh = this.peek();
+      var prevCh = number.charAt(number.length -1);
+      if (ch === 'e' && this.isExpOperator(nextCh)) {           // If number, caught by first if clause
+        number += ch;
+      } else if (this.isExpOperator(ch) && prevCh === 'e' && nextCh && this.isNumber(nextCh)) {
+        number += ch;
+      } else if (this.isExpOperator(ch) && prevCh === 'e' && (!nextCh || !this.isNumber(nextCh))) {
+        throw ("Invalid exponent");
+      } else {
+        break;
+      }
     }
     this.index++;
   }
@@ -42,6 +52,9 @@ Lexer.prototype.peek = function() {
   return this.index < this.text.length - 1 ?
     this.text.charAt(this.index + 1) :
     false;
+};
+Lexer.prototype.isExpOperator = function(ch) {
+  return ch === '-' || ch === '+' || this.isNumber(ch);
 };
 
 function AST(lexer) {  // AST Builder.
